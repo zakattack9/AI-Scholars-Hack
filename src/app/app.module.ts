c7b86cee1e93583cf05ac9585a93e7227f150aeb
Isaiah Harris <harrisfambam@gmail.com>
2018-11-02 21:25:42 -1000

    start header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
-import { HomeComponent } from './Pages/home.component';
+import { HomeComponent } from './Pages/Home/home.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, HomeComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([{ path: '', component: HomeComponent }])
