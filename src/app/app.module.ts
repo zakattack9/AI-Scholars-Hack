3cf26d24dd4b702eb9e23aa0d62881c1ac657159
Isaiah Harris <harrisfambam@gmail.com>
2018-11-03 00:24:32 -1000

    complete routing for three pages

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,14 +5,22 @@ import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/Home/home.component';
 import { HeaderComponent } from './Components/Header/header.component';
 import { StatsComponent } from './Pages/Stats/stats.component';
+import { GradesComponent } from './Pages/Grades/grades.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent, StatsComponent],
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    HeaderComponent,
+    StatsComponent,
+    GradesComponent
+  ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', component: HomeComponent },
-      { path: 'stats', component: StatsComponent }
+      { path: 'stats', component: StatsComponent },
+      { path: 'grades', component: GradesComponent }
     ])
   ],
   providers: [],
