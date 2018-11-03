7fea563f176407a70cb0552f0b67be1f8b813452
Isaiah Harris <harrisfambam@gmail.com>
2018-11-02 21:40:33 -1000

    style header

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,9 +3,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/Home/home.component';
+import { HeaderComponent } from './Components/Header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent],
   imports: [
     BrowserModule,
     RouterModule.forRoot([{ path: '', component: HomeComponent }])
