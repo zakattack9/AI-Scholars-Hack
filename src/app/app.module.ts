b58f10ff74f7bbca8eabd667387093eb5f99ff9c
Isaiah Harris <harrisfambam@gmail.com>
2018-11-03 00:22:12 -1000

    add grades

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,16 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './Pages/Home/home.component';
 import { HeaderComponent } from './Components/Header/header.component';
+import { StatsComponent } from './Pages/Stats/stats.component';
 
 @NgModule({
-  declarations: [AppComponent, HomeComponent, HeaderComponent],
+  declarations: [AppComponent, HomeComponent, HeaderComponent, StatsComponent],
   imports: [
     BrowserModule,
-    RouterModule.forRoot([{ path: '', component: HomeComponent }])
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent },
+      { path: 'stats', component: StatsComponent }
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
