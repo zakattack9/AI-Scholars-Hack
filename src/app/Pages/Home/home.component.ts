98122af32c67a81106256449bde10fc26e9e7869
Zak Sakata <zakattack26111@gmail.com>
2018-11-04 04:31:45 -0700

    finished styling for home and grades

--- src/app/Pages/Home/home.component.ts
+++ src/app/Pages/Home/home.component.ts
@@ -10,21 +10,33 @@ export class HomeComponent implements OnInit {
   subjects = ['Math', 'English', 'Physics'];
   techniques = ['Pomodoro', '20/20/20'];
   ngOnInit() {
-    let dataPoints = [];
+    let dataPoints = [
+      { label: "MON",  y: 10, color: "#13b1d0" },
+      { label: "TUE", y: 15, color: "#13b1d0"  },
+      { label: "WED", y: 25, color: "#13b1d0"  },
+      { label: "THU",  y: 30, color: "#13b1d0"  },
+      { label: "FRI",  y: 28, color: "#13b1d0"  },
+      { label: "SAT",  y: 28, color: "#13b1d0"  },
+      { label: "SUN",  y: 28, color: "#13b1d0"  },
+    ];
     let y = 0;
-    for (var i = 0; i < 8; i++) {
-      y += Math.round(2 - Math.random() * (-5 - 5));
-      dataPoints.push({ y: y });
-    }
+    // for (var i = 0; i < 8; i++) {
+    //   y += Math.round(2 - Math.random() * (-5 - 5));
+    //   dataPoints.push({ y: y });
+    // }
     let chart = new CanvasJS.Chart('chartContainer', {
       zoomEnabled: true,
       animationEnabled: true,
       data: [
         {
           type: 'line',
+          lineColor: "#13b1d0",
           dataPoints: dataPoints
         }
       ],
+      axisY:{
+        suffix: " min"
+      },
       options: {
         legend: {
           display: false
